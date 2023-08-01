aaf6278b17abbd5c1cea4466c285a9015b5224b9
Shanthan Dondapaati <shanthanr.d@gmail.com>
2023-08-01 18:22:36 -0500

    Comment added by shanthan

---
+++ MySql.sql
@@ -0,0 +1,19 @@
+`WqCREATE TABLE SRD_06022023_UK_Accidents_SQOOP
+(Accident_Index VARCHAR(255),
+  Vehicle_Reference INT,
+  Casualty_Reference INT,
+  Casualty_Class INT,
+  Sex_of_Casualty INT,
+  Age_of_Casualty INT,
+  Age_Band_of_Casualty INT,
+  Casualty_Severity INT,
+  Pedestrian_Location INT,
+  #comment added by rahul
+  Pedestrian_Movement INT,
+  Car_Passenger INT,
+  Bus_or_Coach_Passenger INT,
+  Pedestrian_Road_Maintenance_Worker INT,
+  #TEST COMMENT added by SHANTHAN REDDY
+  Casualty_Type INT,
+  Casualty_Home_Area_Type INT)
+;
