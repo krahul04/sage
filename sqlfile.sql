5d6b6ac4aff80f56c986623a38cc739c4e8c0f72
Rahul <rahul248r@gmail.com>
2023-08-01 13:46:01 -0500

    sqoop

---
+++ sqlfile.sql
@@ -0,0 +1,1 @@
+create table rrk_list2(listing_id varchar(255), name varchar(255), host_id varchar(255), host_since varchar(255), host_location varchar(255),host_response_time varchar(255), city varchar(255), latitude varchar(255), longitude varchar(255), review_scores_rating varchar(255));
